b329f59879117667d41f77555f929810d033e53d
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2024-11-04 16:01:30 +0600

    Create a basics setup with react project for my assingment number 8 and navbar design are compleate

---
+++ src/Components/Error/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="text-center">
+                <h4 className="text-4xl font-bold">404</h4>
+                <p className="text-lg">Page not found, please try again!!</p>
+            </div>
+        </div>
+
+    );
+};
+
+export default ErrorPage;
---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+const Root = () => {
+    return (
+        <div>
+            <div className='container mx-auto'>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
